b9200cd70c13157eb0b1d8979c6311a0b7b3a31d
Bilal Ali <bilalal495@outlook.com>
2024-01-12 02:27:23 -0500

    Add max heap to calculate top 3 calories

--- 2022/src/day1.cpp
+++ 2022/src/day1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -12,20 +13,28 @@ int main()
         return 1;
     }
 
-    int maxCal = 0;
-    
+    priority_queue<int> pq;
+
     string line;
     int currCal = 0;
     while (getline(inputData, line)) {
         if (line.size() == 0) {
-            maxCal = max(maxCal, currCal);
+            pq.push(currCal);
             currCal = 0;
         } else {
             currCal += stoi(line);
         }
     }
 
+    int maxCal = pq.top();
+    int top3Cal = 0;
+    for (int i = 0 ; i < 3 ; i++) {
+        top3Cal += pq.top();
+        pq.pop();
+    }
+
     inputData.close();
-    cout << "Maximum calories: " << maxCal<< endl;
-    return maxCal;
+    cout << "Most calories: " << maxCal << endl;
+    cout << "Top 3 most calories: " << top3Cal << endl;
+    return 0;
 }
